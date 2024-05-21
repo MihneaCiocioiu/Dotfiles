d8e1095efa8e0a0b0437bff2b03971fcb019dc14
Mihnea <mihneau@ingy.nl>
2024-05-21 19:36:59 +0200

    Added i3 touchpad configuration file. Changed i3 config for faster browser opening and google-chrome passwords key. (Not working currently).

--- initialSetup.sh
+++ initialSetup.sh
@@ -1,1 +1,4 @@
+sudo ln -s ~/.touchpadConf /etc/X11/xorg.conf.d/90-touchpad.conf
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
 xargs -a initialRequirements.txt -l1 sudo apt install -y
